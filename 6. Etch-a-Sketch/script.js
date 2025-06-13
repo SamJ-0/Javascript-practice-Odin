6c1236dab86f976616091de8b783a2356f0db414
SamJ-0 <210594751+SamJ-0@users.noreply.github.com>
2025-06-13 16:06:59 +0100

    Added a title and button

--- 6. Etch-a-Sketch/script.js
+++ 6. Etch-a-Sketch/script.js
@@ -1,10 +1,19 @@
-createGrid(16);
+    const topPage = document.querySelector(".topPage");
+
+    const button = document.createElement("button");
+    button.classList.add("newGridBtn");
+    button.textContent = "New Grid";
+
+    topPage.appendChild(button);
+
+    const container = document.querySelector(".gridContainer");
+
+    createGrid(16);
 
 function createGrid(num) {
     let gridSize = num*num;
-
+    
     for(let i = gridSize; i > 0; i--) {
-        const container = document.querySelector("#container");
         const newDiv = document.createElement("div");
         newDiv.classList.add("gridPiece");
         container.appendChild(newDiv);
