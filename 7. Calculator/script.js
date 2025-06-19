980874f89472feac9320fca4b222524260f29355
SamJ-0 <210594751+SamJ-0@users.noreply.github.com>
2025-06-19 16:57:37 +0100

    Added an operate function

--- 7. Calculator/script.js
+++ 7. Calculator/script.js
@@ -1,3 +1,7 @@
+const operator = ["+", "-","*", "/"];
+let num1 = 3;
+let num2 = 5;
+
 function add(a, b) {
     return a + b;
 }
@@ -13,3 +17,19 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(a, b, operator) {
+    if(operator === "+") {
+        return add(a, b);
+    } else if(operator === "-") {
+        return subtract(a, b);
+    } else if(operator === "*") {
+        return multiply(a, b);
+    } else if(operator === "/") {
+        return divide(a, b);
+    } else {
+        console.log("Please enter either: +, -, * or /");
+    }
+}
+
+console.log(operate(num1, num2, operator[2]));
