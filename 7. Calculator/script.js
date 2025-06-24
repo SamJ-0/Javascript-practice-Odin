57b151096d4863110027471b1d5bd8ccced70a99
SamJ-0 <210594751+SamJ-0@users.noreply.github.com>
2025-06-24 18:29:09 +0100

    Added eventListener to number butons

--- 7. Calculator/script.js
+++ 7. Calculator/script.js
@@ -1,8 +1,15 @@
-const displayText = document.querySelector(".display");
+const displayText = document.querySelector(".display-text");
+const numbers = document.querySelectorAll(".numbers");
 
 const operator = ["+", "-","*", "/"];
-let num1 = 3;
-let num2 = 5;
+let numbers1 = 0;
+let numbers2 = 5;
+
+let isDefaultDigit = true;
+
+    if (isDefaultDigit === true) {
+        displayText.textContent = 0;
+    }
 
 function add(a, b) {
     return a + b;
@@ -34,4 +41,22 @@ function operate(a, b, operator) {
     }
 }
 
-console.log(operate(num1, num2, operator[2]));
+console.log(operate("3", numbers2, operator[2]));
+
+numbers.forEach(function(numBtns) {
+    numBtns.addEventListener("click", populateDisplay);
+})
+
+function populateDisplay(event) {
+    const numberLength = displayText.textContent.length;
+    
+    isDefaultDigit = false
+
+    if(isDefaultDigit != true && numbers1 === 0) {
+        numbers1 = displayText.textContent = event.target.textContent;
+        console.log(isDefaultDigit);
+    } else if(numbers1 > 0 && numberLength < 12) {
+           numbers1 = displayText.textContent += event.target.textContent;
+           console.log("numbers1 = " + numbers1);
+        }
+}
