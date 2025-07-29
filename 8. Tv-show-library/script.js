c608d9c91cf6862167d37f97e9ba9e2c4914f1bc
SamJ-0 <210594751+SamJ-0@users.noreply.github.com>
2025-07-29 11:11:42 +0100

    Added an add tv show button and starting dialog modal

--- 8. Tv-show-library/script.js
+++ 8. Tv-show-library/script.js
@@ -1,8 +1,15 @@
 const body = document.querySelector(".container");
+const modal = document.querySelector(".modal");
+const openModal = document.querySelector(".add-show-btn");
+
+openModal.addEventListener('click', () => {
+    modal.showModal();
+})
+
 
 const myLibrary = [];
 
-function Show(title, episodes, seasons, releaseDate, watched, id) {
+function Show(title, episodes, seasons, releaseDate, watched, genre, id) {
     if (!new.target) {
         throw Error("You must use the `new` operator to call the constructor");
     }
@@ -12,14 +19,11 @@ function Show(title, episodes, seasons, releaseDate, watched, id) {
     this.seasons = seasons;
     this.releaseDate = releaseDate;
     this.watched = watched;
+    this.genre = genre;
     this.id = id;
 
     this.info = function() {
-        return `${this.title}
-        Episodes: ${this.episodes}
-        Seasons: ${this.seasons}
-        Released: ${this.releaseDate}
-        Seen: ${this.watched}`;
+        return `${this.title}, Episodes: ${this.episodes}, Seasons: ${this.seasons}, Released: ${this.releaseDate}, Seen: ${this.watched}`;
     }
 }
 
@@ -27,18 +31,43 @@ function addShowToLibrary(obj) {
     myLibrary.push(obj);
 }
 
-function addShowToPage(arr) {
+function displayShow(arr) {
     arr.forEach(function(element) {
-        console.log(element);
         const addTvShow = document.createElement("div");
-        const cardContents = document.createElement("p");
+        
+        const cardTitle = document.createElement("p");
+        const cardEpisodes = document.createElement("p");
+        const cardSeasons = document.createElement("p");
+        const cardReleaseDate = document.createElement("p");
+        const cardWatched = document.createElement("p");
+
+        const cardGenreBg = document.createElement("div");
+        const cardGenre = document.createElement("p");
+
         const removeBtn = document.createElement("button");
 
         addTvShow.classList.add("card");
         addTvShow.setAttribute("data-attribute", element.id);
 
-        cardContents.textContent = element.info();
-        addTvShow.appendChild(cardContents);
+        cardTitle.textContent = element.title;
+        cardEpisodes.textContent = `Episodes: ${element.episodes}`;
+        cardSeasons.textContent = `Seasons: ${element.seasons}`;
+        cardReleaseDate.textContent = `Released: ${element.releaseDate}`;
+        cardWatched.textContent = `Seen: ${element.watched}`;
+        cardGenre.textContent = element.genre;
+
+        cardTitle.classList.add("show-title");
+        cardGenreBg.classList.add("show-genre-bg-colour");
+        cardGenre.classList.add("show-genre");
+
+        addTvShow.appendChild(cardTitle);
+        addTvShow.appendChild(cardEpisodes);
+        addTvShow.appendChild(cardSeasons);
+        addTvShow.appendChild(cardReleaseDate);
+        addTvShow.appendChild(cardWatched);
+
+        cardGenreBg.appendChild(cardGenre);
+        addTvShow.appendChild(cardGenreBg);
 
         removeBtn.classList.add("remove-btn");
         removeBtn.textContent = "X";
@@ -50,13 +79,13 @@ function addShowToPage(arr) {
     });
 }
 
-const show1 = new Show("Dexter: Resurrection", 3, "1 season", "11th July 2025", "Watching", crypto.randomUUID());
-const show2 = new Show("Leverage: Redemption", 39, "3 seasons", "22nd October 2021", "Seen", crypto.randomUUID());
-const show3 = new Show("Dexter: Resurrection", 3, "1 season", "11th July 2025", "Watching", crypto.randomUUID());
-const show4 = new Show("Leverage: Redemption", 39, "3 seasons", "22nd October 2021", "Seen", crypto.randomUUID());
+const show1 = new Show("Dexter: Resurrection", 3, "1 season", "11th July 2025", "Watching", "Crime", crypto.randomUUID());
+const show2 = new Show("Leverage: Redemption", 39, "3 seasons", "22nd October 2021", "Seen", "Heist", crypto.randomUUID());
+const show3 = new Show("Dexter: Resurrection", 3, "1 season", "11th July 2025", "Watching", "Crime", crypto.randomUUID());
+const show4 = new Show("Leverage: Redemption", 39, "3 seasons", "22nd October 2021", "Seen", "Heist", crypto.randomUUID());
 
 addShowToLibrary(show1);
 addShowToLibrary(show2);
 addShowToLibrary(show3);
 addShowToLibrary(show4);
-addShowToPage(myLibrary);
+displayShow(myLibrary);
